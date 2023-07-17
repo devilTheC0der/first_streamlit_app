b2cb1daf0f884d98f7b23e3c2222cc743827e9df
devilTheC0der <118396424+devilTheC0der@users.noreply.github.com>
2023-07-17 12:35:20 +0530

    Update streamlit_app.py
    Added try and except in previous commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ streamlit.dataframe(fruits_to_show)
 # import requests
 streamlit.header('FruityVice Fruit Advice !')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please Select a fruit to get information')
   else:
